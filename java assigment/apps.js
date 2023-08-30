eea17f5a909a6127c9da7ff06c995eb81194330a
umerabbasii890 <140706534+umerabbasii890@users.noreply.github.com>
2023-08-30 05:27:35 -0700

    Add files via upload

---
+++ java assigment/apps.js
@@ -0,0 +1,51 @@
+// QUESTION 1
+
+// var arr = [[]];
+// console.log(arr)
+
+// QUESTION 2
+
+// var arr = [[0,1,2,3,1,0,1,2,2,1,0,1]];
+// console.log(arr)
+
+// QUESTION 3 
+
+// for(i = 1; i <= 10; i++){
+// console.log(i)
+// }
+
+// QUESTION 4
+
+// var table = 2;
+// for(var i=1; i<=15; i++){
+// console.log("Tables===>", prompt + "x" + i +"=" + table * i)
+// }
+
+// QUESTION 5
+
+// var fruit = ["mango","apple","peach","banana"];
+// for(var i= 0;i <= 3; i++){
+// console.log("Element at Index",fruit[i]);
+// }
+
+// QUESTION 6
+
+// var num = [1,2,3,4,5,6,7,8,9,10];
+// var min = num[0];
+// for(var i = 0; i = num.length; i++){
+// if(num[i] < min){
+//     min = num[i];
+// }
+// }
+// console.log(min);
+
+// QUESTION 7
+
+// var num = [1,2,3,4,5,6,7,8,9,10];
+// var min = num[1];
+// for(var i = 0; i = num.length; i++){
+// if(num[i] > min){
+//     min = num[i];
+// }
+// }
+// console.log(min);
